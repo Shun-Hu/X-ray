4a9a84b7ace914bb448d88616f6c2612fc0f22a0
huxuezhang <70133914+Shun-Hu@users.noreply.github.com>
2023-04-12 14:04:38 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -652,7 +652,8 @@ function show_error_log() {
 
 function bbr_boost_sh() {
   [ -f "tcp.sh" ] && rm -rf ./tcp.sh
-  wget -N --no-check-certificate "https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
+  bash <(curl -sL https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcp.sh)
+#  wget -N --no-check-certificate "https://raw.githubusercontent.com/ylx2016/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
 }
 
 function mtproxy_sh() {
