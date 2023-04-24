b303e343a45cfe755d596ce96e89ede086f9db11
huxuezhang <70133914+Shun-Hu@users.noreply.github.com>
2023-04-24 02:29:10 +0000

    Update auto_shutdown_nginx.sh

--- auto_shutdown_nginx.sh
+++ auto_shutdown_nginx.sh
@@ -3,7 +3,11 @@
 # 等待30天（以秒为单位）
 # sleep 2592000
 # sleep 1296000
-nohup sleep 120
+#nohup sleep 120
 
 # 停止Nginx服务
-sudo systemctl stop nginx
+#sudo systemctl stop nginx
+
+
+#或者用这个命令
+nohup sh -c "sleep 2592000 && sudo systemctl stop nginx" > sleep.log &
